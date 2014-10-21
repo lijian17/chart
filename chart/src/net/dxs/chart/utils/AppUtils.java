fc06b66af4c2d9b8ce700b0cfb1e36ee640107c1
lijian17 <374452668@qq.com>
2014-10-21 09:58:35 +0800

    工具类

---
+++ chart/src/net/dxs/chart/utils/AppUtils.java
@@ -0,0 +1,10 @@
+package net.dxs.chart.utils;
+
+public class AppUtils {
+	/** 密度 **/
+	public static float DENSITY;
+	/** 屏幕宽度 **/
+	public static int WIDTH;
+	/** 屏幕高度 **/
+	public static int HEIGHT;
+}
